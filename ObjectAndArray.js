fd57c756494f6111cf4ec31ac9dc412ee788f1b9
Dmitry <gerasimovdima03@yandex.ru>
2024-03-04 00:47:32 +0300

    objectarr

---
+++ ObjectAndArray.js
@@ -0,0 +1,25 @@
+var phone = new Object();
+phone.name = "iPhone";
+phone.model = "XR";
+phone.memory = 128;
+
+let info =[
+    phone.name,
+    phone.model,
+    phone.memory,
+];
+for (var i = 0; i<3; i++){
+    console.log(info[i]);
+}
+
+let arr = [
+    1,
+    4,
+    2,
+    9,
+    5,
+];
+ arr.sort();
+ for (var i = 0; i<arr.length; i++){
+    console.log(arr[i]);
+}
